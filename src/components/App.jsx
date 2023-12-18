835d9463effd28219c6d3558901af7d5218de377
Dimch93 <dimch93@ukr.net>
2023-12-18 13:39:09 +0200

    home7-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { PhonebookForm } from './PhonebookForm/PhonebookForm';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Filter } from './Filter/Filter';
+import { fetchContacts } from '../redux/operations';
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <>
       <h1>Phonebook</h1>
